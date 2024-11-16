9ac1378099a6f02cb54d6adda7713ae50686ec36
Ambar Itzel <zarzaambar52@gmail.com>
2024-11-15 21:19:16 -0600

    Update Apuntadores1.c

--- Apuntadores1.c
+++ Apuntadores1.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+//declaracion de variables
 int *p, y;
+//declaramos las funciones 
 void func(){
     int x=40;
     p=&x;
@@ -12,6 +13,7 @@ int main(void){
     func();
     y=*p;
     *p=25;
+    //imprimimos el resultado
     printf("El valor de y es %d \nEl valor de *p es %d\nEl valor de p es %p", y, *p, p);
     
 }
