6ff0e81f980e5db3a700bfb26f17ebf39ac3cde0
Ambar Itzel <zarzaambar52@gmail.com>
2024-11-16 06:23:32 -0600

    Update AreaCirculo.c

--- AreaCirculo.c
+++ AreaCirculo.c
@@ -1,11 +1,13 @@
+//mandamos llamar las librerias quevamos a ultilizar
 #include <stdio.h>
 #include <stdlib.h>
-
+//empieza a ejecutarse nuestro programa 
 int main(){
+	//declaramos las variables 
     double d=10;
 	double pi= 3.1416;
 	double area = pi * (d/2)*(d/2);
-	
+	//imprimimos el area 
 	printf("%f",area);
 	
 	printf ("\n");
