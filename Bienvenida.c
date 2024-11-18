6860ab709b466dea0946b11d982f1a9d4b3665df
Ambar Itzel <zarzaambar52@gmail.com>
2024-11-17 21:35:33 -0600

    Update Bienvenida.c

--- Bienvenida.c
+++ Bienvenida.c
@@ -1,8 +1,11 @@
+// importamos las librerias a ocupar 
 #include <stdio.h>
 #include <stdlib.h>
-
+//  tenemos nuestro metodo main
 int main(){
-	printf("Bienvenida es un gusto compilar para Ana \n");
+	//tenemo nuestro mensaje a imprimir 
+	printf("Bienvenida es un gusto compilar para Ambat \n");
+	// le ponemos un pause para que se detenga nuestro programita 
 	system("Pause");
 	return 0;
 	
