d42ff9a296e649f7ad80ca0830aa0de4199aced6
Ambar Itzel <zarzaambar52@gmail.com>
2024-11-18 06:23:52 -0600

    Update Vector.c

--- Vector.c
+++ Vector.c
@@ -5,7 +5,9 @@
 int main(){
     // declaramos nuestras variables de tipo entero las cuales van a ser in vector 
     int vector[10];
+    // lo inivializamos en cero 
     int i=0;
+    //tenemos nuetro ciclo while
     while(i<10){
         vector[i]=10;
         printf("%d \n", vector[i]);
