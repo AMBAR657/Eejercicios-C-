5e03b68c7b3b075287fc0171a75ed866b53baed4
Ambar Itzel <zarzaambar52@gmail.com>
2024-11-18 06:20:58 -0600

    Update Vector.c

--- Vector.c
+++ Vector.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 //tenemos nuetra fincion principal 
 int main(){
+    // declaramos nuestras variables de tipo entero las cuales van a ser in vector 
     int vector[10];
     int i=0;
     while(i<10){
