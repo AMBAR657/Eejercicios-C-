bc3255538d904bed5b9e54a6bc09730111d2b333
Ambar Itzel <zarzaambar52@gmail.com>
2024-11-26 20:54:25 -0600

    Update Apuntadores.c

--- Apuntadores.c
+++ Apuntadores.c
@@ -7,6 +7,7 @@ int main(){
 int x=17,y;
 int *p;
 p=&x;
+  //imprimimos el texyo y la variable 
 printf("EL valor de x es %p\n", *p);
 y=*p+3;
 // esta es la forma en la que se imprime y se muetra el texto 
