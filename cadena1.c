4417070f54e430b21f221c2c162d06cec9217c8d
Ambar Itzel <zarzaambar52@gmail.com>
2024-12-02 07:11:35 -0600

    Update cadena1.c

--- cadena1.c
+++ cadena1.c
@@ -6,6 +6,7 @@ int main(){
     //vamos a tener nuetra cadena de tipo char
     char cad[]="Es una cadena";
     //vamos a imprimir con espacios 
+    //vamos a imprimir lo que contenga cad
     printf("%s \n", cad);
     system("Pause");
     return 0;
