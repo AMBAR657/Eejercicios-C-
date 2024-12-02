03eb7d55242bd19fb24b5e8ec29b3591486d7ef7
Ambar Itzel <zarzaambar52@gmail.com>
2024-12-02 07:10:13 -0600

    Update cadena1.c

--- cadena1.c
+++ cadena1.c
@@ -1,7 +1,7 @@
 //mandamos llamar las librerias 
 #include <stdio.h>
 #include <stdlib.h>
-
+//ahora tenemos nuetro metodo main
 int main(){
     char cad[]="Es una cadena";
     printf("%s \n", cad);
